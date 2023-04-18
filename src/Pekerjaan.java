e57f0999343c500ba2a020eabce5b2839eba9745
frendysanusi05 <frendyvivo5@gmail.com>
2023-04-18 20:41:00 +0700

    Add Edible, Purchasable, Pekerjaan (Frendy done)

---
+++ src/Pekerjaan.java
@@ -0,0 +1,35 @@
+public class Pekerjaan {
+    private String nama;
+    private int gaji = 0;
+
+    /* Constructor */
+    public Pekerjaan(String nama) {
+        this.nama = nama;
+        switch(nama) {
+            case "Badut Sulap":
+                this.gaji = 15;
+                break;
+            case "Koki":
+                this.gaji = 30;
+                break;
+            case "Polisi":
+                this.gaji = 35;
+                break;
+            case "Programmer":
+                this.gaji = 45;
+                break;
+            case "Dokter":
+                this.gaji = 50;
+                break;
+        }
+    }
+
+    /* Getter */
+    public String getNama() {
+        return nama;
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
+}
