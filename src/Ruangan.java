1f907441dd55c24bfb4985d39c7290e50c7779a4
Aapiiz <99938025+Aapiiz@users.noreply.github.com>
2023-04-26 02:13:37 +0700

    perbaikan Ruangan

--- src/Ruangan.java
+++ src/Ruangan.java
@@ -6,6 +6,7 @@ public class Ruangan extends Rumah{
     private Point titikRuangan;
     private ArrayList<Furniture> daftarObjek;
     private int capacity;
+    private boolean[][] isAvailable = new boolean[6][6];
 
     //constructor
     public Ruangan(String namaRuangan, Rumah rumah, Point titikRuangan){
@@ -14,6 +15,12 @@ public class Ruangan extends Rumah{
         this.namaRuangan = namaRuangan;
         this.titikRuangan = titikRuangan;
         this.capacity = 36;
+
+        for(int i=0; i<6; i++){
+            for(int j=0; j<6; j++){
+                isAvailable[j][i] = true;
+            }
+        }
     }
 
     //getter
@@ -42,18 +49,92 @@ public class Ruangan extends Rumah{
     }
 
     //method
-    public boolean isAvailable(Furniture barang){
-        return (barang.getLebar() * barang.getPanjang() <= capacity);
+    public void addObjectToList(Furniture obj){
+        daftarObjek.add(obj);
     }
 
-    //pake arraylist (dalemnya furniture) -> identifikasinya di ruangan
-    public void addObject(String namaBarang){
-        try {
-            Furniture barang = new Furniture(namaBarang);
-            daftarObjek[daftarObjek.length] = barang;
-            capacity -= barang.getPanjang() * barang.getLebar();
-        } catch (Exception e) {
-            e.printStackTrace();
+    //dipake buat mempermudah user untuk memilih daerah yg mau diisi (?)
+    //user bakalan lbh tergambarkan kondisi buat mau nambahin atau engga furniture di ruangan itu kalo dikasi gambaran kek gini gasi (?)
+    public void printRuangan(){
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+                if(isAvailable[j][i]){
+                    System.out.print("O");
+                }else{
+                    System.out.print("X");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Bagian bertanda 'X' adalah area yang terisi oleh furniture, adapun 'O' digunakan untuk menyatakan area yang masih kosong");
+    }
+
+    public void insertObjectToRuangan(String namaBarang, Point koordinat /*koordinat barang yg mau disimpen*/, Ruangan ruangan) throws Exception{
+        Furniture obj = new Furniture(namaBarang);
+        int panjang = obj.getPanjang();
+        int lebar = obj.getLebar();
+        boolean opsi1 = (koordinat.getX() >= ruangan.getXRuangan() - 3 && koordinat.getY() >= ruangan.getYRuangan() - 3 && koordinat.getX() + panjang <= 3 + ruangan.getXRuangan() && koordinat.getY() + lebar <= 3 + ruangan.getYRuangan());
+        boolean opsi2 = (koordinat.getX() >= ruangan.getXRuangan() - 3 && koordinat.getY() >= ruangan.getYRuangan() - 3 && koordinat.getX() + lebar <= 3 + ruangan.getXRuangan() && koordinat.getY() + panjang <= 3 + ruangan.getYRuangan());
+        
+        if(opsi1){
+            int tempX = koordinat.getX();
+            int tempY = koordinat.getY();
+            boolean found = false;
+            while(!found && tempX<tempX+panjang){
+                while(!found && tempY<tempY+lebar){
+                    if(isAvailable[tempY][tempX]){
+                        tempX++;
+                        tempY++;
+                    } else{
+                        found = true;
+                    }
+                    
+                }
+            }
+
+            if(!found){
+                //update Map Furniture pada Ruangan
+                tempX = koordinat.getX();
+                tempY = koordinat.getY();
+                while(tempX<tempX+panjang){
+                    while(tempY<tempY+lebar){
+                        isAvailable[tempY][tempX] = false;
+                    }
+                }
+                this.addObjectToList(obj);
+            }
+
+        } else if (opsi2){
+            int tempX = koordinat.getX();
+            int tempY = koordinat.getY();
+            boolean found = false;
+            while(!found && tempX<tempX+lebar){
+                while(!found && tempY<tempY+panjang){
+                    if(isAvailable[tempY][tempX]){
+                        tempX++;
+                        tempY++;
+                    } else{
+                        found = true;
+                    }
+                    
+                }
+            }
+
+            if(!found){
+                //update Map Furniture pada Ruangan
+                tempX = koordinat.getX();
+                tempY = koordinat.getY();
+                while(tempX<tempX+lebar){
+                    while(tempY<tempY+panjang){
+                        isAvailable[tempY][tempX] = false;
+                    }
+                }
+                this.addObjectToList(obj);
+            }
+
+
+        } else {
+            System.out.println("Maaf, koordinat yang anda masukkan tidak memiliki cukup ruang pada Ruangan " + getIDRuangan());
         }
     }
 }
