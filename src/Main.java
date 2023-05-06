6f1667e4e683ef0d3a44d8c8db0abe1d9692614c
Attariq Muhammad Azhar <attariq03.azhar@gmail.com>
2023-05-06 17:46:58 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -182,7 +182,7 @@ public class Main {
         boolean exitGame = false;
 
         System.out.print("Generating Sim");
-        Animasi, hiraukan
+        
         Thread.sleep(1000);
         System.out.print(".");
         System.out.print(".");
